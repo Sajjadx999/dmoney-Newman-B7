89827b3e9c6e5a041aa9f8bb74324a8cf04ecbf6
Sajjadx999 <sajjad.rahman808080@gmail.com>
2023-05-28 07:51:22 +0600

    upload

--- report.js
+++ report.js
@@ -1,12 +1,12 @@
 const newman = require('newman'); 
 newman.run({ 
- collection: require("./Collection/dmoneyapi.json"), 
- //collection: 'https://api.postman.com/collections/18755112-d5baae90-3913-4b52-a88e-9e80de970b9c?access_key=PMAT-01H1BBGFJKJ3TRXKWYHDBXRDJE', 
+ //collection: require("./b2b7APIdmoney.json"), 
+ collection: 'https://api.postman.com/collections/18755112-ef04e5b6-5f95-4519-a72a-89a5244a4f26?access_key=PMAT-01H1FZPNQ359XWBBC16KJYCRD6', 
  reporters: 'htmlextra',
  iterationCount: 1,
  reporter: { 
  htmlextra: { 
- export: './Reports/report.html', // If not specified, the file  will be written to `newman/` in the current working directory.  } 
+ export: './Reports1/report.html', // If not specified, the file  will be written to `newman/` in the current working directory.  } 
  } 
   }
 }, function (err) { 
